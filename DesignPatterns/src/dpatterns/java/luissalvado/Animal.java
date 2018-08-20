f899e653416fe552bd2c976d84718ae4efe8ac3d
Luís Miguel <luismiguelsalvado@gmail.com>
2018-08-20 22:10:00 +0100

    add DesignPatterns project folder

---
+++ DesignPatterns/src/dpatterns/java/luissalvado/Animal.java
@@ -0,0 +1,30 @@
+package dpatterns.java.luissalvado;
+
+public class Animal {
+	
+	private String name;
+	private int weight;
+	private String sound;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public String getSound() {
+		return sound;
+	}
+	public void setSound(String sound) {
+		this.sound = sound;
+	}
+	
+	
+
+}
